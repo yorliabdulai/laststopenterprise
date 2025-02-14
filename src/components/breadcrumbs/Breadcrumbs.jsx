865c0f884358a58b5afffb29b6dfae505b3e0865
yorliabdulai <yorliabdulai1@gmail.com>
2025-02-14 10:55:46 +0000

    feat: add documentation for Breadcrumbs component

--- src/components/breadcrumbs/Breadcrumbs.jsx
+++ src/components/breadcrumbs/Breadcrumbs.jsx
@@ -1,6 +1,20 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
 
+
+
+/**
+ * The Breadcrumbs component renders a section with a Home link and
+ * a link for each of the `type`, `checkout`, and `stripe` props.
+ * If a prop is not provided, the corresponding link will not be rendered.
+ * Each link is rendered with a bold font and a font size of xl or 3xl.
+ * Each link is also given a class of "text-secondary-content" if the link is active.
+ * The links are separated by a slash.
+ * The section is given a background color of primary-content and a min height of 20px on mobile and 36px on desktop.
+ * The section is also given a flexbox layout with the links centered horizontally.
+ * The section is given a max width of 1200px and is centered horizontally on desktop.
+ * The section is given a padding of 2px on mobile and 6px on desktop.
+ */
 const Breadcrumbs = ({ type, checkout, stripe }) => {
 	const activeLink = ({ isActive }) => (isActive ? "text-secondary-content " : null);
 
