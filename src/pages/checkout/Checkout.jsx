4c5f570bde373c305ab8830ce7628a8140c37fa7
yorliabdulai <yorliabdulai1@gmail.com>
2025-02-18 13:47:52 +0000

    fix: update order amount calculation to use totalAmount directly for accurate processing

--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -75,7 +75,7 @@ const Checkout = () => {
                 })),
                 email,
                 shippingAddress,
-                amount: totalAmount / 100,  // Convert to the smallest unit (e.g., kobo)
+                amount: totalAmount,  // Convert to the smallest unit (e.g., kobo)
                 description: `Payment of ${formatPrice(totalAmount/100)} from ${email}`,
                 orderStatus: "Pending",  // Set initial status to "Pending"
             };
